2c37fbd46ea189b5f9e0b1ee449cf66f596bc532
Nam Nguyen <hoangnam123@gmail.com>
2014-08-31 13:00:37 -0700

    First commit

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// If the player hits the trigger...
+		if(col.gameObject.tag == "Player")
+		
+		{
+			Destroy (gameObject);	
+		
+		}
+
+	}
+}
